4f4278f3c0864ec251ab0b688d70785255bbd6ce
Hector Cortez <updown.input@gmail.com>
2022-08-04 11:56:07 -0300

    initial commit

---
+++ src/test/java/com/icons/iconslab/IconsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icons.iconslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
