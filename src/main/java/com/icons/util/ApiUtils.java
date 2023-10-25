8bddc331ce1bdb473baf2f2d7ac46baaa71fdefd
Héctor Cortez <updown.input@gmail.com>
2023-10-24 21:26:59 -0300

    refactor: utility class created and new properties added

---
+++ src/main/java/com/icons/util/ApiUtils.java
@@ -0,0 +1,30 @@
+package com.icons.util;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.regex.Pattern;
+
+public class ApiUtils {
+    public static final String CONTINENT_BASE_URL = "/api/v1/continents";
+
+    public static final String COUNTRY_BASE_URL = "/api/v1/countries";
+
+    public static final String ICON_BASE_URL = "/api/v1/icons";
+
+    public static final String URI_RESOURCE = "/{id}";
+
+    public static final Date VALID_TOKEN_TIME = new Date(System.currentTimeMillis() + 1000 * 60 * 60);
+
+    private static final Pattern UUID_REGEX_PATTERN = Pattern
+            .compile("^[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?$");
+
+    public static final DateTimeFormatter OF_PATTERN = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+
+    public static final int ELEMENTS_PER_PAGE = 10;
+
+    public static final String REGEX_PATTERN = "/(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/";
+
+    public static boolean isNotValidUUID(String uuid) {
+        return !UUID_REGEX_PATTERN.matcher(uuid).matches();
+    }
+}
